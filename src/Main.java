2e787947152466ce83272a6e728806b4e8c476dc
Iskhak-69 <iskhak.koichubaev@alatoo.edu.kg>
2023-11-23 10:52:53 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int[] locations = {2, 3, 4};
+        int[] weights = {150, 250, 313};
+
+
+
+    }
+}
