143fd375921e8d5f82103ac1e18ee88b8aef7570
Iskhak-69 <iskhak.koichubaev@alatoo.edu.kg>
2023-11-23 11:29:11 +0600

    sodi

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,33 @@ public class Main {
         int[] locations = {2, 3, 4};
         int[] weights = {150, 250, 313};
 
+        int attemps = 0;
+
+        while(attemps < 5){
+            System.out.println("Enter numbers ");
+            int[] numbers = guessNumbers();
+
+            for (int i = 0; i < locations.length; i++) {
+                for(int j = 0; j < numbers.length; j++){
+                    if(locations[i] != numbers[j]){
+                        System.out.println("fsdf");
+                    }
+                }
+
+            }
+
+        }
+
 
 
     }
+    public static int[] guessNumbers(){
+        Scanner scanner = new Scanner(System.in);
+        int guess1 = scanner.nextInt();
+        int guess2 = scanner.nextInt();
+        int guess3 = scanner.nextInt();
+        return new int[]{guess1, guess2, guess3};
+
+    }
+
 }
